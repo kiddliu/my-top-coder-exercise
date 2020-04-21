b3aa50e6b9a1b813a04c290e146be10c0fff8d1e
Kidd Liu <kiddliu.sz@gmail.com>
2020-04-22 00:39:11 +0800

    feat(grafixmask): add some functions

--- recursion/srm-211-div-i-level-2-grafixmask/ts/index.ts
+++ recursion/srm-211-div-i-level-2-grafixmask/ts/index.ts
@@ -1,5 +1,18 @@
 export default class GrafixMask {
     sortedAreas(rectangles: string[]): number[] {
-        return [];
+        const panel = new Array(400).fill(false).map(() => new Array(600).fill(false));
+        this.markRectangles(panel, rectangles);
+
+        const result: number[] = [];
+        panel.forEach((row, ri) => row.forEach((column, ci) => result.push(this.colorEmpty(panel, ri, ci))));
+        return result;
+    }
+
+    private markRectangles(panel: boolean[][], rectangles: string[]): void {
+        
+    }
+
+    private colorEmpty(panel: boolean[][], row, column): number {
+        return 0;
     }
 }
